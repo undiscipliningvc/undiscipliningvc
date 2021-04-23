094c52b1d19402438c2fc723552eda2cc9a3fdff
Adrian Wisnicki <awisnicki@yahoo.com>
2021-04-23 15:17:01 -0500

    add GitHub update scripts

---
+++ bash/update_github.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+readonly PROGNAME=$(basename $0)
+readonly PROGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly ARGS="$@"
+
+cd -- "$(dirname "$BASH_SOURCE")"
+
+git fetch --all
+git checkout dev
+git pull
+git push
+git checkout stage
+git merge dev
+git push
+git checkout dev
